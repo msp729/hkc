396a598f4f30c0afee6dfec69f4b50efb791def8
Nate Brown <n.b.nrb99@icloud.com>
2024-01-12 01:55:26 -0500

    removed completion

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,20 @@ import Calc.Command (Command (..), Fun (..), Var (..))
 import Calc.Common (Ctx (a, ans, b, c, f, g, h), ci)
 import Calc.Parse.Command (command)
 import Calc.Parse.Expr (eval)
-import Control.Monad.Trans.Class (lift)
-import System.Console.Haskeline (InputT, defaultSettings, outputStr, outputStrLn, runInputT, getInputLine)
-import System.IO (isEOF)
+import System.Console.Haskeline (InputT, Settings (..), defaultSettings, outputStrLn, runInputT, getInputLine, noCompletion)
+import System.Console.Haskeline.Completion ()
 import Text.Megaparsec (errorBundlePretty, runParser)
 import Prelude hiding (getLine)
 import Data.Text (pack)
 import Calc.N
 
+ds, st :: Settings IO
+ds = defaultSettings
+st = ds {complete = noCompletion}
+
 main :: IO ()
 main = do
-    runInputT defaultSettings $ loop ci
+    runInputT st $ loop ci
 
 loop :: Ctx -> InputT IO ()
 loop ctx = do
